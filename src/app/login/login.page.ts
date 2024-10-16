417c6d8aa4fecb87464b74ef6018295c02db72a5
Ibai05 <ibaicalvocuesta2005@gmail.com>
2024-10-16 12:28:16 +0200

    sd

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -9,7 +9,18 @@ export class LoginPage implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  onSubmit() {
+    // Aquí puedes agregar la lógica para verificar el usuario y la contraseña
+    console.log('Usuario:', this.username);
+    console.log('Contraseña:', this.password);
 
+    // Simulación de inicio de sesión exitoso
+    if (this.username === 'admin' && this.password === '1234') {
+      // Navegar a la página principal o a la siguiente página
+      this.router.navigate(['/home']);
+    } else {
+      // Manejar el error de inicio de sesión
+      alert('Credenciales inválidas. Inténtalo de nuevo.');
+    }
+  }
 }
