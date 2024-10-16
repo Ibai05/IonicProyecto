2e76843b7cef70a382916f5bd67a5bf51733db6b
Ibai05 <ibaicalvocuesta2005@gmail.com>
2024-10-16 12:32:44 +0200

    cs

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -9,5 +9,28 @@ export class LoginPage implements OnInit {
 
   constructor() { }
 
+  import { Component, OnInit } from '@angular/core';
+
+  @Component({
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
+  })
+  export class LoginPage implements OnInit {
+    username: string; // Define username
+    password: string; // Define password
+
+    constructor() {}
+
+    ngOnInit() {
+      // Inicialización si es necesario
+    }
+
+    onSubmit() {
+      // Lógica para manejar el inicio de sesión
+      console.log('Username:', this.username);
+      console.log('Password:', this.password);
+    }
+  }
 
 }
