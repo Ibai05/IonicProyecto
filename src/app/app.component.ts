06170ba49eff15e266202e46bde778d8e770c7ba
Ibai05 <ibaicalvocuesta2005@gmail.com>
2024-10-16 12:42:31 +0200

    asa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ export class AppComponent {
   public appPages = [
     { title: 'Inicio', url: '/folder/inbox', icon: 'mail'},
     { title: 'Alta', url: '/folder/alta',icon: 'mail'},
-    { title: 'Login', url: '/folder/login',icon: 'mail'},
+    { title: 'Login', url: '/login',icon: 'mail'},
     
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
