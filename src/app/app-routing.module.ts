dee9b3ffd13c25f70550b3e6ab85995e14f13dfc
Ubuntu <ubuntu@ip-172-31-92-123.ec2.internal>
2024-10-16 08:02:52 +0000

    Añadida nueva pagina Alta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'alta',
+    loadChildren: () => import('./alta/alta.module').then( m => m.AltaPageModule)
   }
 ];
 
