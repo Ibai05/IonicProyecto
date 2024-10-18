cf2632250c30cc03113d2befb102ab07c5866830
Ibai05 <ibaicalvocuesta2005@gmail.com>
2024-10-18 10:33:02 +0200

    gfdg

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CursoService } from '.curso.service'; 
+import { CursoService } from '../curso.service'; // Asegúrate de que la ruta sea correcta
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-folder',
@@ -9,22 +10,24 @@ import { CursoService } from '.curso.service';
 })
 export class FolderPage implements OnInit {
   public folder!: string;
-  public cursos: any[] = [];
+  public cursos!: any[]; 
   private activatedRoute = inject(ActivatedRoute);
-  private cursoService = inject(CursoService); 
+  private cursoService = inject(CursoService);
 
   constructor() {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    this.cargarCursos(); 
+  }
 
-    // Cargar los cursos al inicializar
+  cargarCursos() {
     this.cursoService.getCursos().subscribe(
       (data: any[]) => {
         this.cursos = data; 
       },
       (error: any) => {
-        console.error('Error al cargar cursos:', error);
+        console.error('Error al cargar cursos', error);
       }
     );
   }
