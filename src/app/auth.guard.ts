3dc2ba99cf7b825034ee9a0fc8aaa457f8947e53
Ibai05 <ibaicalvocuesta2005@gmail.com>
2024-10-21 18:29:28 +0200

    sds

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,24 +1,39 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { AuthService } from './auth.service';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Storage } from '@ionic/storage-angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private router: Router, private storage: Storage) {}
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): boolean {
-    const user = this.authService.getCurrentUser(); 
+  async canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Promise<boolean | UrlTree> {
+    
+    // Obtener la información del usuario desde LocalStorage o SessionStorage
+    const usuario = await this.storage.get('usuario');
 
-    if (user) {
-      return true; 
-    } else {
-      this.router.navigate(['/login']); 
-      return false; 
+    // Si no existe el usuario, redirigir al login
+    if (!usuario) {
+      this.router.navigate(['/login']);
+      return false;
     }
+
+    // Comprobar si la ruta requiere que el usuario sea admin
+    const requiereAdmin = next.data['requiereAdmin'] || false;
+
+    // Si la ruta requiere ser admin, comprobar si el usuario es admin
+    if (requiereAdmin && !usuario.admin) {
+      // Si no es admin, redirigir a la página de inicio o donde prefieras
+      this.router.navigate(['/inicio']);
+      return false;
+    }
+
+    // Si el usuario es válido y cumple con los requisitos, permitir el acceso
+    return true;
   }
 }
