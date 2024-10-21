848eea342ca514894bd71e219a4336812f7a8e2e
Ibai05 <ibaicalvocuesta2005@gmail.com>
2024-10-21 08:28:30 +0200

    sdsad

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http'; // Importa HttpClient para hacer peticiones
+import { Router } from '@angular/router'; // Para redireccionar al inicio
 
 @Component({
   selector: 'app-login',
@@ -10,20 +12,36 @@ export class LoginPage implements OnInit {
   username: string = ''; 
   password: string = ''; 
 
-  constructor() {}
+  constructor(private http: HttpClient, private router: Router) {} // Inyecta HttpClient y Router
 
-  ngOnInit() {
-    
-  }
+  ngOnInit() {}
 
   onSubmit(form: NgForm) {
     if (form.valid) {
-      
-      console.log('Username:', this.username);
-      console.log('Password:', this.password);
-      
+      const loginData = {
+        nombre: this.username,
+        contrasena: this.password
+      };
+
+      this.http.post('http://44.194.177.243:8100/usuario', loginData)
+        .subscribe(
+          (response: any) => {
+            if (response.success) {
+              localStorage.setItem('user', JSON.stringify(response.user));
+              // o SessionStorage
+              // sessionStorage.setItem('user', JSON.stringify(response.user));
+
+              this.router.navigate(['/folder']);
+            } else {
+              console.log('Credenciales incorrectas');
+            }
+          },
+          (error: any) => {
+            console.log('Error en la autenticación:', error);
+          }
+        );
     } else {
-      console.log('Form is invalid');
+      console.log('Formulario inválido');
     }
   }
 }
