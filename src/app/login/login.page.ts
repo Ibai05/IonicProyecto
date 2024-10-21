c7b5e9ba2b3877a0bc586898e4fe7e3979fcb5aa
Ibai05 <ibaicalvocuesta2005@gmail.com>
2024-10-21 08:14:38 +0200

    w

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -7,21 +7,21 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  username: string = ''; // Inicializar username
-  password: string = ''; // Inicializar password
+  username: string = ''; 
+  password: string = ''; 
 
   constructor() {}
 
   ngOnInit() {
-    // Inicialización si es necesario
+    
   }
 
   onSubmit(form: NgForm) {
     if (form.valid) {
-      // Lógica para manejar el inicio de sesión
+      
       console.log('Username:', this.username);
       console.log('Password:', this.password);
-      // Aquí puedes agregar la lógica para autenticar al usuario, como llamar a un servicio
+      
     } else {
       console.log('Form is invalid');
     }
