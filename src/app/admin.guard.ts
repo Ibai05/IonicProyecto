51312a7bee14b4384ef573e2cbb3d10fe52f4945
Ibai05 <ibaicalvocuesta2005@gmail.com>
2024-10-21 18:44:06 +0200

    dsd

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
+
   constructor(private router: Router) {}
 
-  canActivate(): boolean {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+    
     const user = JSON.parse(localStorage.getItem('user') || '{}');
 
     if (user && user.admin) {
-      return true; 
+      return true; // El usuario es admin, puede acceder
     }
 
+    // Redirigir si no es admin
     this.router.navigate(['/folder/inbox']);
     return false;
   }
