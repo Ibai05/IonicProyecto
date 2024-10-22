3094b9972afe7203cf1479f1faa6cec51f49a791
Ibai05 <ibaicalvocuesta2005@gmail.com>
2024-10-22 09:32:48 +0200

    sad

--- src/app/alumno/alumno.page.ts
+++ src/app/alumno/alumno.page.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AlumnoService } from '../alumno.service'; // Asegúrate de que la ruta sea correcta
+import { CursoService } from '../curso.service';
 
 @Component({
   selector: 'app-alumno',
-  templateUrl: './alumno.page.html',
+  templateUrl: './alumno.page.html', // Asegúrate de que el nombre del archivo HTML sea correcto
   styleUrls: ['./alumno.page.scss'],
 })
 export class AlumnoPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
