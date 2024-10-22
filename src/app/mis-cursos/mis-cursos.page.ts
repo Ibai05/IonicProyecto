6dda17d448f32758a4932e6c033551ea4963e770
Ibai05 <ibaicalvocuesta2005@gmail.com>
2024-10-22 10:26:34 +0200

    sad

--- src/app/mis-cursos/mis-cursos.page.ts
+++ src/app/mis-cursos/mis-cursos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CursoService } from '../curso.service';
 
 @Component({
   selector: 'app-mis-cursos',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mis-cursos.page.scss'],
 })
 export class MisCursosPage implements OnInit {
+  misCursos: any[] = [];
+  usuarioId: number; // Obtén el ID del usuario de donde lo necesites (ejemplo, de LocalStorage)
 
-  constructor() { }
+  constructor(private cursoService: CursoService) {}
 
   ngOnInit() {
+    // Aquí debes asignar el ID del usuario, por ejemplo:
+    this.usuarioId = Number(localStorage.getItem('usuarioId'));
+    this.cargarMisCursos();
   }
 
+  cargarMisCursos() {
+    this.cursoService.getMisCursos(this.usuarioId).subscribe(
+      (data: any[]) => {
+        this.misCursos = data;
+      },
+      (error: any) => {
+        console.error('Error al cargar los cursos del usuario', error);
+      }
+    );
+  }
 }
