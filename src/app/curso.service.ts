e8414652410380ea3be75d76aa9c1f6b41ca18af
Ibai05 <ibaicalvocuesta2005@gmail.com>
2024-10-22 08:34:03 +0200

    sdsd

--- src/app/curso.service.ts
+++ src/app/curso.service.ts
@@ -18,4 +18,8 @@ export class CursoService {
     return this.http.post(`${this.apiUrl}/asignar-alumno`, { alumno_id: alumnoId, curso_id: cursoId });
 
   }
+
+  getCursosPorUsuario(usuarioId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/alumno/cursos/${usuarioId}`);
+  }
 }
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { CursoService } from '../curso.service'; // Asegúrate de que la ruta sea correcta
-import { Observable } from 'rxjs';
+import { CursoService } from '../curso.service';
+import { AuthService } from '../auth.service'; // Asegúrate de importar tu AuthService
 
 @Component({
   selector: 'app-folder',
@@ -10,21 +10,25 @@ import { Observable } from 'rxjs';
 })
 export class FolderPage implements OnInit {
   public folder!: string;
-  public cursos!: any[]; 
+  public cursos!: any[];
   private activatedRoute = inject(ActivatedRoute);
   private cursoService = inject(CursoService);
+  private authService = inject(AuthService); // Inyectar AuthService
 
   constructor() {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
-    this.cargarCursos(); 
+    this.cargarCursos();
   }
 
   cargarCursos() {
-    this.cursoService.getCursos().subscribe(
+    const usuario = this.authService.getUsuario(); // Obtener usuario del localStorage
+    const usuarioId = usuario.id;
+
+    this.cursoService.getCursosPorUsuario(usuarioId).subscribe(
       (data: any[]) => {
-        this.cursos = data; 
+        this.cursos = data;
       },
       (error: any) => {
         console.error('Error al cargar cursos', error);
