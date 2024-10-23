11221e511b26afbeb4242b82edee7ee45a0f81ef
Ibai05 <ibaicalvocuesta2005@gmail.com>
2024-10-23 08:57:27 +0200

    dsad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,31 +11,22 @@ export class AppComponent {
     { title: 'Inicio', url: '/folder/inbox', icon: 'mail' },
     { title: 'Alta', url: '/alta', icon: 'mail' },
     { title: 'Login', url: '/login', icon: 'mail' },
+    { title: 'Borrar', url: '/borrar', icon: 'mail' },
   ];
-
+  
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   public userName: string = '';
   public userSurname: string = '';
-  public isAdmin: boolean = false; 
 
   constructor(private authService: AuthService) {
-    this.authService.user$.subscribe((user: { nombre: string; apellido: string; admin: boolean }) => {
+    this.authService.user$.subscribe((user: { nombre: string; apellido: string; }) => {
       if (user) {
         this.userName = user.nombre || '';
         this.userSurname = user.apellido || '';
-        this.isAdmin = user.admin || false; 
       } else {
         this.userName = '';
         this.userSurname = '';
-        this.isAdmin = false;
       }
     });
-  }
-
-  getAppPages() {
-    if (this.isAdmin) {
-      return [...this.appPages, { title: 'Borrar', url: '/borrar', icon: 'trash' }];
-    }
-    return this.appPages;
   }
 }
