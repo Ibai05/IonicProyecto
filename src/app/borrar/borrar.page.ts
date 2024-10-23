36165cc9bbe5b96c741c7ce772204f3b8f10140b
Ibai05 <ibaicalvocuesta2005@gmail.com>
2024-10-23 08:11:07 +0200

    dfd

--- src/app/borrar/borrar.page.ts
+++ src/app/borrar/borrar.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AlumnoService } from '../alumno.service';
+import { CursoService } from '../curso.service';
 
 @Component({
   selector: 'app-borrar',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BorrarPage implements OnInit {
 
-  constructor() { }
+  alumnos: any[] = [];
+  cursos: any[] = [];
+  selectedAlumno: number | null = null;
+
+  constructor(private alumnoService: AlumnoService, private cursoService: CursoService) {}
 
   ngOnInit() {
+    this.cargarAlumnos();
   }
+
+  cargarAlumnos() {
+    this.alumnoService.getAlumnos().subscribe(
+      (data: any[]) => {
+        this.alumnos = data;
+      },
+      (error: any) => {
+        console.error('Error al cargar alumnos', error);
+      }
+    );
+  }
+
 
 }
