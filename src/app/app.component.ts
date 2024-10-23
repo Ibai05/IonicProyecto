4d3277361959491337948b34c4079a5e4e7d8c00
Ibai05 <ibaicalvocuesta2005@gmail.com>
2024-10-23 09:02:07 +0200

    sds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,22 +10,28 @@ export class AppComponent {
   public appPages = [
     { title: 'Inicio', url: '/folder/inbox', icon: 'mail' },
     { title: 'Alta', url: '/alta', icon: 'mail' },
-    { title: 'Login', url: '/login', icon: 'mail' },
-    { title: 'Borrar', url: '/borrar', icon: 'mail' },
+    { title: 'Login', url: '/login', icon: 'mail' }
   ];
-  
+
+  public adminPages = [
+    { title: 'Borrar', url: '/borrar', icon: 'trash' }
+  ];
+
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   public userName: string = '';
   public userSurname: string = '';
+  public isAdmin: boolean = false; 
 
   constructor(private authService: AuthService) {
-    this.authService.user$.subscribe((user: { nombre: string; apellido: string; }) => {
+    this.authService.user$.subscribe((user: { nombre: string; apellido: string; admin: boolean; }) => {
       if (user) {
         this.userName = user.nombre || '';
         this.userSurname = user.apellido || '';
+        this.isAdmin = user.admin; 
       } else {
         this.userName = '';
         this.userSurname = '';
+        this.isAdmin = false; 
       }
     });
   }
